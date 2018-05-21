50329cea58c396f134c067622392deef2e7b9959
David Federspiel <david@federnet.com>
2018-05-21 09:06:25 -0400

    feat: design implementation

--- src/js/client/logo.js
+++ src/js/client/logo.js
@@ -1,68 +1,60 @@
-var t = 0.28;
+var t = 0.15;
 var t1, t2, t3, t4, t5, t6, t7, t8;
 
 function doStuff() {
   t1 = new TimelineMax();
   t1.to(".st1", t, {
-    morphSVG: ".st0",
     transformOrigin: "center center",
-    scale: 1,
+    scale: 0,
     ease: Power2.easeInOut
   });
 
   t2 = new TimelineMax();
-  t2.to(".st2", t, {
-    morphSVG: ".st0",
+  t2.to(".st2", t * 1.1, {
     transformOrigin: "center center",
-    scale: 1,
+    scale: 0,
     ease: Power2.easeInOut
   });
 
   t3 = new TimelineMax();
-  t3.to(".st3", t, {
-    morphSVG: ".st0",
+  t3.to(".st3", t * 1.2, {
     transformOrigin: "center center",
-    scale: 1,
+    scale: 0,
     ease: Power2.easeInOut
   });
 
   t4 = new TimelineMax();
-  t4.to(".st4", t, {
-    morphSVG: ".st0",
+  t4.to(".st4", t * 1.3, {
     transformOrigin: "center center",
-    scale: 1,
+    scale: 0,
     ease: Power2.easeInOut
   });
 
   t5 = new TimelineMax();
-  t5.to(".st5", t, {
-    morphSVG: ".st0",
+  t5.to(".st5", t * 1.4, {
     transformOrigin: "center center",
-    scale: 1,
+    scale: 0,
     ease: Power2.easeInOut
   });
 
   t6 = new TimelineMax();
-  t6.to(".st6", t, {
-    morphSVG: ".st0",
+  t6.to(".st6", t * 1.5, {
     transformOrigin: "center center",
-    scale: 1,
+    scale: 0,
     ease: Power2.easeInOut
   });
 
   t7 = new TimelineMax();
-  t7.to(".st7", t, {
-    morphSVG: ".st0",
+  t7.to(".st7", t * 1.6, {
     transformOrigin: "center center",
-    scale: 1,
+    scale: 0,
     ease: Power2.easeInOut
   });
 
   t8 = new TimelineMax();
-  t8.to(".st8", t, {
-    morphSVG: ".st0",
+  t8.to(".st8", t * 1.7, {
     transformOrigin: "center center",
-    scale: 1,
+    scale: 0,
     ease: Power2.easeInOut
   });
 }
@@ -84,7 +76,7 @@ $("#logo svg").click(() => {
 
 var isCollapsed = false;
 
-$(window).resize(function(){
+function setLogoState(){
   if($(window).width() < 500 && !isCollapsed){
     doStuff();
     isCollapsed = true;
@@ -100,4 +92,12 @@ $(window).resize(function(){
     t8.reverse();
     isCollapsed = false;
   }
+}
+
+$(window).resize(function(){
+  setLogoState();
 });
+
+$(document).ready(function(){
+  setLogoState();
+})
