726fb1851a1baa39adcd72d85bc8faa7e5dc87c8
David Federspiel <david@federnet.com>
2018-05-21 09:06:01 -0400

    clean: removed cached files

---
+++ src/js/client/scroll-reveal.js
@@ -0,0 +1,18 @@
+window.sr = ScrollReveal({ reset: true });
+
+var defaultOptions = {
+    duration: 500, 
+    delay: 50, 
+    origin: "top"
+}
+
+function setReveal(animationClass, options){
+    $('.reveal-wrapper' + animationClass).each(function(idx, item){
+        sr.reveal($('> *', item), options);
+    })
+}
+
+setReveal('.from-top', { ...defaultOptions, origin: 'top' });
+setReveal('.from-right', { ...defaultOptions, origin: 'right' });
+setReveal('.from-bottom', { ...defaultOptions, origin: 'bottom' });
+setReveal('.from-left', { ...defaultOptions, origin: 'left' });
